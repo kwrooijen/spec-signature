205d849e4f5da009da8888f96bfc502729632103
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2019-12-14 16:41:57 +0100

    Add :deploy-repositories

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :url "https://github.com/kwrooijen/spec-sig"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]])
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                    :username :env/clojars_user
+                                    :password :env/clojars_pass
+                                    :sign-releases false}]])
