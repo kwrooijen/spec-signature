b7f45eb71b2ceb83eddde2ef373f2c973fda0f01
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2020-01-05 14:27:46 +0100

    Bump version to 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spec-signature "0.1.0"
+(defproject spec-signature "0.2.0"
   :description "Write spec signatures for functions"
   :url "https://github.com/kwrooijen/spec-signature"
   :license {:name "EPL-2.0"
